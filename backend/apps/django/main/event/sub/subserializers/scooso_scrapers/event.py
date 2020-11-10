312f49da5d108d254924ccb7cb21f2c356f77e00
myzel394 <myzel394.xyllian@gmail.com>
2020-11-10 20:48:54 +0100

    Event import bugfix

--- backend/apps/django/main/event/sub/subserializers/scooso_scrapers/event.py
+++ backend/apps/django/main/event/sub/subserializers/scooso_scrapers/event.py
@@ -1,3 +1,6 @@
+from django_common_utils.libraries.handlers.mixins import BaseHandlerMixin
+from django_common_utils.libraries.utils import ensure_iteration
+
 from apps.django.utils.serializers import GetOrCreateSerializerMixin
 from ....models import Event
 
@@ -12,3 +15,17 @@ class EventScoosoScraperSerializer(GetOrCreateSerializerMixin):
         fields = [
             "title", "start_datetime", "end_datetime"
         ]
+    
+    def create(self, validated_data):
+        handlers = Event.handlers()["title"]
+        title = validated_data.pop("title")
+        for handler in ensure_iteration(handlers, lambda element: issubclass(element.__class__, BaseHandlerMixin)):
+            title = handler.handle(title)
+        
+        return Event.objects.get_or_create(
+            title__iexact=title,
+            defaults={
+                "title": title,
+                **validated_data
+            },
+        )[0]
