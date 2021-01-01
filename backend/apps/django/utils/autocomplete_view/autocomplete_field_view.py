f942c2e4d4c1093b3d2a9ad8c80ca15d7a4fc2dd
myzel394 <miguel.kra@gmail.com>
2021-01-01 14:33:44 +0100

    Autocomplete improvements

--- backend/apps/django/utils/autocomplete_view/autocomplete_field_view.py
+++ backend/apps/django/utils/autocomplete_view/autocomplete_field_view.py
@@ -1,6 +1,6 @@
 from abc import ABC
 
-from django.db.models import F
+from django.db.models import Count, F
 
 from apps.django.utils.autocomplete_view.autocomplete_view import AutocompleteView, T
 from apps.django.utils.autocomplete_view.type_hints import DefaultResultType
@@ -9,14 +9,30 @@ from apps.django.utils.autocomplete_view.type_hints import DefaultResultType
 class AutocompleteFieldView(AutocompleteView, ABC):
     field: str
     max_results: int = 20
+    return_all_when_empty: bool = True
     
     def get_qs(self, user: "User") -> T:
         return self.model.objects.from_user(user)
     
     def filter_qs(self, qs: T, query: str) -> T:
-        return qs.only(self.field).filter(**{
+        if query == "" and self.return_all_when_empty:
+            return qs
+        
+        filters = {
             f"{self.field}__trigram_similar": query
-        })
+        }
+        
+        return qs \
+            .only(self.field) \
+            .filter(**filters)
     
     def serialize_qs(self, qs: T) -> list[DefaultResultType]:
-        return qs.annotate(text=F(self.field)).values("id", "text").distinct()[:self.max_results]
+        count = qs \
+            .values(self.field) \
+            .annotate(count=Count(self.field)) \
+            .order_by(f"-{self.field}")
+        
+        return count \
+                   .annotate(text=F(self.field)) \
+                   .values("text", "count") \
+                   .distinct()[:self.max_results]
