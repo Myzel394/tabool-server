79a78470d382dbd9db92ccb56691eea7ace01768
myzel394 <miguel.kra@gmail.com>
2021-01-05 16:08:49 +0100

    Course list fetch api added

--- backend/apps/django/main/lesson/sub/subviews/apis/course.py
+++ backend/apps/django/main/lesson/sub/subviews/apis/course.py
@@ -1,3 +1,6 @@
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+
 from apps.django.utils.viewsets import RetrieveFromUserMixin
 from ....models import Course
 from ....serializers import CourseDetailSerializer
@@ -7,6 +10,8 @@ __all__ = [
 ]
 
 
-class CourseViewSet(RetrieveFromUserMixin):
+class CourseViewSet(viewsets.mixins.ListModelMixin, RetrieveFromUserMixin):
+    filter_backends = [SearchFilter]
+    search_fields = ["subject__name", "course_number", "teacher__lastname"]
     serializer_class = CourseDetailSerializer
     model = Course
