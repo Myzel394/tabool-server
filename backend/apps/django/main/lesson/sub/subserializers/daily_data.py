b233574a76c443e179557ffebf3bbb0e5a352243
myzel394 <miguel.kra@gmail.com>
2021-01-09 16:46:19 +0100

    get_date default function for DailyDataSerializer added

--- backend/apps/django/main/lesson/sub/subserializers/daily_data.py
+++ backend/apps/django/main/lesson/sub/subserializers/daily_data.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from rest_framework import serializers
 
@@ -7,6 +7,16 @@ __all__ = [
 ]
 
 
+def get_date():
+    today = date.today()
+    weekday = today.weekday()
+    
+    if weekday >= 5:
+        today += timedelta(days=7 - weekday)
+    
+    return today
+
+
 class DailyDataSerializer(serializers.Serializer):
-    date = serializers.DateField(default=lambda: date.today())
+    date = serializers.DateField(default=get_date)
     max_future_days = serializers.IntegerField(min_value=0, max_value=30, default=5)
