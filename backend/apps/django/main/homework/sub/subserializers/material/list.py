1f64fa1bd4ba00af8070a0c4a589c86efc24e783
myzel394 <miguel.kra@gmail.com>
2021-01-28 21:33:23 +0100

    ListMaterialSerializer bugfixes

--- backend/apps/django/main/homework/sub/subserializers/material/list.py
+++ backend/apps/django/main/homework/sub/subserializers/material/list.py
@@ -1,5 +1,13 @@
+from typing import *
+
+from rest_framework import serializers
+
 from apps.django.main.lesson.public.serializer_fields.lesson import LessonField
+from apps.django.main.school_data.sub.subserializers.subject import SubjectSerializer
 from .base import BaseMaterialSerializer
+
+if TYPE_CHECKING:
+    from ....models import Material
 
 __all__ = [
     "ListMaterialSerializer"
@@ -9,7 +17,12 @@ __all__ = [
 class ListMaterialSerializer(BaseMaterialSerializer):
     class Meta(BaseMaterialSerializer.Meta):
         fields = [
-            "lesson", "name", "added_at", "id"
+            "lesson", "subject", "name", "added_at", "id"
         ]
     
     lesson = LessonField()
+    
+    subject = serializers.SerializerMethodField()
+    
+    def get_subject(self, instance: "Material"):
+        return SubjectSerializer(instance=instance.lesson.lesson_data.course.subject, context=self.context).data
