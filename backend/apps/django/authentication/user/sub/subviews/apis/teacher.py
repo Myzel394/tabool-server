f618520495092a8b84764828a05546140df6b24a
myzel394 <miguel.kra@gmail.com>
2021-02-13 13:13:31 +0100

    teacher search functionality added

--- backend/apps/django/authentication/user/sub/subviews/apis/teacher.py
+++ backend/apps/django/authentication/user/sub/subviews/apis/teacher.py
@@ -1,17 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from apps.django.authentication.user.models import Teacher
 from apps.django.authentication.user.sub.subserializers.teacher import DetailTeacherSerializer
+from apps.django.utils.permissions import AuthenticationAndActivePermission, IsStudent
 
 __all__ = [
     "TeacherViewSet"
 ]
 
-from apps.django.utils.permissions import AuthenticationAndActivePermission, IsStudent
-
 
 class TeacherViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [AuthenticationAndActivePermission & IsStudent]
+    
+    filter_backends = [SearchFilter]
+    search_fields = ["user__first_name", "user__last_name"]
+    
     model = Teacher
     serializer_class = DetailTeacherSerializer
     
