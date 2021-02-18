282a9da21515601d24737cd9a0a939d0050cc7aa
myzel394 <miguel.kra@gmail.com>
2021-02-18 13:38:56 +0100

    subject color constants fix

--- backend/apps/django/main/course/constants.py
+++ backend/apps/django/main/course/constants.py
@@ -3,9 +3,10 @@ SUBJECT_COLORS_MAPPING = {
     "englisch": "#14A5FF",
     "mathe": "#FF4834",
     "deutsch": "#2072FF",
-    "französisch": "#FFDA28",
+    "französisch": "#0050a4",
     "latein": "#73FFF2",
-    "spanisch": "#EAFF00",
+    "spanisch": "#f1bf00",
+    "italienisch": "#008c45",
     
     "informatik": "#1E67FF",
     
@@ -15,11 +16,12 @@ SUBJECT_COLORS_MAPPING = {
     
     "sozialkunde_erdkunde": "#BE10FF",
     "sozialkunde": "#FF0021",
-    "eerdunde": "#CE8F24",
+    "erdkunde": "#CE8F24",
     
     "katholische_religion": "#D70AFF",
     "ethik": "#D70AFF",
     "evangelische_religion": "#D70AFF",
+    "religion": "#D70AFF",
     
     "musik": "#FFFB00",
     "darstellendes_spiel": "#48FFF7",
