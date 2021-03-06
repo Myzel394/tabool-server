b42917141165270c038a9d0c9bab0d62f6d484a3
myzel394 <miguel.kra@gmail.com>
2021-03-06 20:53:31 +0100

    made material and submission accessible to student and teachers

--- backend/apps/django/main/homework/routers.py
+++ backend/apps/django/main/homework/routers.py
@@ -9,10 +9,12 @@ __all__ = [
 student_router = SimpleRouter()
 student_router.register("homework", StudentHomeworkViewSet, basename="homework")
 student_router.register("submission", SubmissionViewSet, basename="submission")
+student_router.register("material", MaterialViewSet, basename="material")
 student_router.register("classbook", ClassbookViewSet, basename="classbook")
 
 teacher_router = SimpleRouter()
 teacher_router.register("homework", TeacherHomeworkViewSet, basename="homework")
+teacher_router.register("submission", SubmissionViewSet, basename="submission")
 teacher_router.register("material", MaterialViewSet, basename="material")
 teacher_router.register("classbook", ClassbookViewSet, basename="classbook")
 
