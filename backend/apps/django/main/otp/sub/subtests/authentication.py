3e475ec28e1903ac539ee922441556e13f15fcc2
myzel394 <miguel.kra@gmail.com>
2021-03-16 19:02:21 +0100

    tests fix for experimental flag

--- backend/apps/django/main/otp/sub/subtests/authentication.py
+++ backend/apps/django/main/otp/sub/subtests/authentication.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 from apps.django.main.otp.models.otp import OTP
 from apps.django.utils.tests_mixins import ClientTestMixin, UserTestMixin
+from project import settings
 
 
 class AuthenticationOTPTest(UserTestMixin, ClientTestMixin):
@@ -25,6 +26,9 @@ class AuthenticationOTPTest(UserTestMixin, ClientTestMixin):
         return otp
     
     def test_otp_valid(self):
+        if settings.IS_EXPERIMENTAL:
+            return
+        
         otp = self.request()
         
         response = self.client.post("/api/auth/login/", {
@@ -36,6 +40,9 @@ class AuthenticationOTPTest(UserTestMixin, ClientTestMixin):
         self.assertStatusOk(response.status_code)
     
     def test_otp_invalid(self):
+        if settings.IS_EXPERIMENTAL:
+            return
+        
         self.request()
         
         response = self.client.post("/api/auth/login/", {
@@ -48,6 +55,9 @@ class AuthenticationOTPTest(UserTestMixin, ClientTestMixin):
         self.assertEqual("Ungültiges OTP.", response.data["otp_key"])
     
     def test_otp_valid_expired(self):
+        if settings.IS_EXPERIMENTAL:
+            return
+        
         otp = self.request()
         
         # Make otp expired
@@ -64,6 +74,9 @@ class AuthenticationOTPTest(UserTestMixin, ClientTestMixin):
         self.assertEqual("Dieses OTP ist abgelaufen. Es wurde dir ein neues zugeschickt.", response.data["otp_key"])
     
     def test_otp_not_existing(self):
+        if settings.IS_EXPERIMENTAL:
+            return
+        
         response = self.client.post("/api/auth/login/", {
             "email": self.user.email,
             "password": self.user_password,
@@ -73,6 +86,9 @@ class AuthenticationOTPTest(UserTestMixin, ClientTestMixin):
         self.assertEqual(401, response.status_code)
     
     def test_otp_deletes_after_success(self):
+        if settings.IS_EXPERIMENTAL:
+            return
+        
         otp = self.request()
         
         response = self.client.post("/api/auth/login/", {
@@ -87,6 +103,9 @@ class AuthenticationOTPTest(UserTestMixin, ClientTestMixin):
         self.assertNotEqual(otp, new_otp)
     
     def test_no_new_otp_created_after_wrong_input(self):
+        if settings.IS_EXPERIMENTAL:
+            return
+        
         otp = self.request()
         
         response = self.client.post("/api/auth/login/", {
