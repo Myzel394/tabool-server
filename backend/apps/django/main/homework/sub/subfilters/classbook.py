eea1361b512d6d1dd0222d5357b735d3178cb91a
myzel394 <miguel.kra@gmail.com>
2021-04-06 20:52:53 +0200

    filter model fix

--- backend/apps/django/main/homework/sub/subfilters/classbook.py
+++ backend/apps/django/main/homework/sub/subfilters/classbook.py
@@ -2,7 +2,7 @@ from django_filters import rest_framework as filters
 
 from apps.django.main.course.public import model_names as course_names
 from apps.django.main.timetable.mixins import LessonFilterSetMixin
-from ...models import Homework
+from ...models import Classbook
 
 __all__ = [
     "ClassbookFilterSet"
@@ -11,9 +11,8 @@ __all__ = [
 
 class ClassbookFilterSet(LessonFilterSetMixin):
     class Meta:
-        model = Homework
+        model = Classbook
         fields = {
-            "due_date": ["lte", "gte"],
             "presence_content": ["trigram_similar"],
             "online_content": ["trigram_similar"],
             "video_conference_link": ["exact", "isnull"]
