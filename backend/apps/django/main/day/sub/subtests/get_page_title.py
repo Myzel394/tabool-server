04f77d8d487747d2be7d244881db28fe41dab7c5
myzel394 <miguel.kra@gmail.com>
2021-05-15 16:02:12 +0200

    improvements

--- backend/apps/django/main/day/sub/subtests/get_page_title.py
+++ backend/apps/django/main/day/sub/subtests/get_page_title.py
@@ -17,52 +17,52 @@ class GetPageTitleTest(UserTestMixin):
     invalid_status_code_mock = Mock(
         status_code=invalid_status_code_mock_status_code,
     )
-
+    
     def setUp(self) -> None:
         self.Login_student()
-
+    
     @patch("apps.django.main.day.sub.subviews.api.get_page_title.requests.Session.get")
     def test_get_valid(self, mock):
         mock.return_value = self.valid_mock
-
+        
         response = self.client.get("/api/data/get-page-title/", {
             "url": "https://bbb-schulen.rlp.net"
         }, content_type="application/json")
         self.assertStatusOk(response.status_code)
         self.assertEqual(self.valid_mock_title, response.data["title"])
-
+    
     @patch("apps.django.main.day.sub.subviews.api.get_page_title.requests.Session.get")
     def test_get_invalid_html(self, mock):
         mock.return_value = self.invalid_html_mock
-
+        
         response = self.client.get("/api/data/get-page-title/", {
             "url": "https://bbb-schulen.rlp.net"
         }, content_type="application/json")
         self.assertStatusNotOk(response.status_code)
-
+    
     @patch("apps.django.main.day.sub.subviews.api.get_page_title.requests.Session.get")
     def test_get_failed_status_code(self, mock):
         mock.return_value = self.invalid_status_code_mock
-
+        
         response = self.client.get("/api/data/get-page-title/", {
             "url": "https://bbb-schulen.rlp.net"
         }, content_type="application/json")
         self.assertStatusNotOk(response.status_code)
         self.assertEqual(self.invalid_status_code_mock_status_code, response.data["status_code"])
-
+    
     @patch("apps.django.main.day.sub.subviews.api.get_page_title.requests.Session.get")
     def test_cant_pass_not_listed_url(self, mock):
         mock.return_value = self.invalid_status_code_mock
-
+        
         response = self.client.get("/api/data/get-page-title/", {
             "url": "https://example.com"
         }, content_type="application/json")
         self.assertStatusNotOk(response.status_code)
-
+    
     @patch("apps.django.main.day.sub.subviews.api.get_page_title.requests.Session.get")
     def test_cant_pass_not_listed_protocol(self, mock):
         mock.return_value = self.invalid_status_code_mock
-
+        
         response = self.client.get("/api/data/get-page-title/", {
             "url": "file://bbb-schulen.rlp.net"
         }, content_type="application/json")
